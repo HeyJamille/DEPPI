3f0dccbc6e62ad4aa75e24108d888aaf91c60459
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-08-04 15:59:20 -0300

    [ADD] components galeria and contacts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ContentsComponent } from '../../components/contents/contents.component'; // Ajuste o caminho conforme necessário
 import { AboutComponent } from '../../components/about/about.component';
+import { GalleryComponent } from '../../components/gallery/gallery.component';
+import { ContactsComponent } from '../../contacts/contacts.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  imports: [ContentsComponent, AboutComponent] // Importe diretamente aqui
+  imports: [ContentsComponent, AboutComponent, GalleryComponent, ContactsComponent] // Importe diretamente aqui
 })
 export class HomeComponent {
 }
