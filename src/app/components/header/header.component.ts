cd47410026b2f715ea752f0691918c22e948f9b2
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-08-07 20:07:04 -0300

    [UPDATE] responsive header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
+  standalone: true,
+  imports: [CommonModule]
 })
 export class HeaderComponent {
+  isScrolled: boolean = false;
+  isMenuOpen: boolean = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY;
+    this.isScrolled = scrollPosition > 0;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.setStyle(document.body, 'overflow', 'auto');
+    }
+  }
 }
